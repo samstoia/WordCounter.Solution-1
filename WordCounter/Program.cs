df5bec52803f6655ef3628d3f2db7c35d054cd7c
LondresRi <lunsford.sk@gmail.com>
2019-02-15 12:12:26 -0800

    add flavor text to Program.cs

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -9,13 +9,13 @@ namespace WordCounter
     public static void Main()
     {
       Counter counter = new Counter();
-      Console.WriteLine("Intro");
-      Console.WriteLine("Instructions");
-      Console.WriteLine("Enter word");
+      Console.WriteLine("\nWelcome to the word counter!\n");
+      Console.WriteLine("-----\nThis simple program will ask you for a word and a sentence. It will then compare the two and return the number of occurences your word appears in your sentence.\n-----");
+      Console.WriteLine("\nPlease enter a word:");
       string userWord = counter.Filter(Console.ReadLine().ToLower());
-      Console.WriteLine("Enter sentence");
+      Console.WriteLine("\nNow please write out a sentence to compare:");
       string userSentence = counter.Filter(Console.ReadLine().ToLower());
-      Console.WriteLine("Magic time!");
+      Console.WriteLine("\n-----\nGreat! Time to do some counting!\n-----\n");
       int result = new Counter().Compare(userWord, userSentence);
       if (result == 0)
       {
@@ -23,7 +23,7 @@ namespace WordCounter
       }
       else
       {
-        Console.WriteLine($"{char.ToUpper(userWord[0])}{userWord.Substring(1)} has {result} matches found in '{userSentence}'!");
+        Console.WriteLine($"{char.ToUpper(userWord[0])}{userWord.Substring(1)} has {result} matches found in '{userSentence}', congratulations!");
       }
     }
 
