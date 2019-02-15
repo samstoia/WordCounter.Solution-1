fafd26cf26b916f3c3bf3c533131071014d0cbab
LondresRi <lunsford.sk@gmail.com>
2019-02-15 12:24:25 -0800

    modify program.cs text for styling, add more characters to Filter methods to prevent user smart alecking

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -9,21 +9,20 @@ namespace WordCounter
     public static void Main()
     {
       Counter counter = new Counter();
-      Console.WriteLine("\nWelcome to the word counter!\n");
-      Console.WriteLine("-----\nThis simple program will ask you for a word and a sentence. It will then compare the two and return the number of occurences your word appears in your sentence.\n-----");
-      Console.WriteLine("\nPlease enter a word:");
+      Console.WriteLine("\nWelcome to the word counter!\n-----\n\nThis simple program will ask you for a word and a sentence. It will then compare the two and return the number of occurences your word appears in your sentence.\n\n-----\n\nPlease enter a word:");
       string userWord = counter.Filter(Console.ReadLine().ToLower());
       Console.WriteLine("\nNow please write out a sentence to compare:");
       string userSentence = counter.Filter(Console.ReadLine().ToLower());
-      Console.WriteLine("\n-----\nGreat! Time to do some counting!\n-----\n");
+      Console.WriteLine("\n-----\n\nGreat! Time to do some counting! Please any key to continue.\n\n-----");
+      Console.ReadKey();
       int result = new Counter().Compare(userWord, userSentence);
       if (result == 0)
       {
-        Console.WriteLine($"{char.ToUpper(userWord[0])}{userWord.Substring(1)} has NO matches found in '{userSentence}'...");
+        Console.WriteLine($"{char.ToUpper(userWord[0])}{userWord.Substring(1)} has NO matches found in '{userSentence}'...\n");
       }
       else
       {
-        Console.WriteLine($"{char.ToUpper(userWord[0])}{userWord.Substring(1)} has {result} matches found in '{userSentence}', congratulations!");
+        Console.WriteLine($"{char.ToUpper(userWord[0])}{userWord.Substring(1)} has {result} matches found in '{userSentence}', congratulations!\n");
       }
     }
 
