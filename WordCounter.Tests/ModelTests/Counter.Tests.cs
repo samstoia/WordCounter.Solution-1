76d58cf758a1f3975d0dd70bfde2f8fa23990da5
LondresRi <lunsford.sk@gmail.com>
2019-02-15 10:52:17 -0800

    add filtering test using regex, then non-regex methods

--- WordCounter.Tests/ModelTests/Counter.Tests.cs
+++ WordCounter.Tests/ModelTests/Counter.Tests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using WordCounter.Models;
 
 namespace WordCounter.Tests
@@ -9,11 +10,35 @@ namespace WordCounter.Tests
   public class CounterTests
   {
     [TestMethod]
-    public void UserInputTest_IsThisAUsersInput_True()
+    public void UserInputTest_IsThisAUsersInput_Bool_True()
     {
       string userInput = "*Ca$T!1?";
       bool result = userInput is string;
       Assert.AreEqual(true, result);
+    }
+
+    [TestMethod]
+    public void Filter_FilterUserInputForWordCharactersWithRegex_String_True()
+    {
+      string userInput = ("*Ca$T!1?").ToLower();
+      Regex Filter = new Regex(@"[^a-zA-Z]");
+      string result = Filter.Replace(userInput, "");
+      Assert.AreEqual("cat", result);
+    }
+
+    [TestMethod]
+    public void Filter_FilterUserInputForWordCharactersWithArray_String_True()
+    {
+      string userInput = ("*Ca$T!1?").ToLower();
+      string[] filter = {"0","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","?","<",">",".",","};
+      foreach(string c in filter)
+      {
+        if (userInput.Contains(c))
+        {
+          userInput = userInput.Replace(c, "");
+        }
+      }
+      Assert.AreEqual("cat", userInput);
     }
   } // end of CounterTests
 
